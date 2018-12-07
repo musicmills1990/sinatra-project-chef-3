82735f2c35af465e1644a1440f561d3ef2aec43a
Marc Mills <mdmills1990@gmail.com>
2018-12-07 22:09:27 +0000

    builds new ingredients form, built some ingredient_controller routes

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,20 @@
 class IngredientsController < ApplicationController
+
+  get '/ingredients' do
+    if logged_in?
+      @ingredients = Ingredient.all
+      erb :"ingredients/index"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/ingredients/new' do
+    if logged_in?
+      @ingredients = Ingredient.all
+      erb :"ingredients/new"
+    else
+      redirect "/login"
+    end
+  end
 end
