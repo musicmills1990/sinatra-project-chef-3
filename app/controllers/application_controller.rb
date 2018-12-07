122e34dfa407d4e8b2e589530c176a6bd0fd54a8
Marc Mills <mdmills1990@gmail.com>
2018-12-07 00:35:50 +0000

    Set up file structure, adds config & environment code

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+    register Sinatra::Flash
+    end
+
+  get '/' do
+    erb :"index"
+  end
+
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
+end
