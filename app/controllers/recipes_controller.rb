8df772f6d5e9002f581e1fbd542f0af96eda6e77
Marc Mills <mdmills1990@gmail.com>
2018-12-08 20:00:32 +0000

    adds edit and delete forms and controllers for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -50,5 +50,58 @@ class RecipesController < ApplicationController
     end
   end
 
+  get '/recipes/:id/edit' do
+    if logged_in?
+      @recipe = Recipe.find_by_id(params[:id])
+      erb :"recipes/edit"
+    else
+      flash[:errors] = "You have to be logged in to view this page. Please log in."
+      redirect '/login'
+    end
+  end
 
+  patch '/recipes/:id' do
+    if logged_in?
+      if params == "" || params == " "
+        flash[:errors] = "You can't empty out a recipe"
+        redirect "recipe/#{params[:id]}/edit"
+      else
+        @recipe = Recipe.find_by_id(params[:id])
+        if @recipe # && @recipe.user == current_user
+          if @recipe.update(content: params[:content], recipe_first_ingredient: params[:recipe_first_ingredient], recipe_second_ingredient: params[:recipe_second_ingredient], recipe_third_ingredient: params[:recipe_third_ingredient], recipe_fourth_ingredient: params[:recipe_fourth_ingredient], instructions: params[:instructions])
+            flash[:message] = "Recipe successfully updated!"
+            redirect "/recipes/#{@recipe.id}"
+          else
+            flash[:errors] = "You can't empty out a recipe."
+            redirect "/recipes/#{@recipe.id}/edit"
+          end
+        else
+          flash[:errors] = "You can only edit your own recipes"
+          redirect "/recipes"
+        end
+      end
+    else
+      flash[:errors] = "You have to be logged in to view this page. Please log in."
+      redirect "/login"
+    end
+  end
+
+
+
+  delete '/recipes/:id/delete' do
+    if logged_in?
+      @recipe = Recipe.find_by_id(params[:id])
+      if @recipe #&& @recipe.user == current_user --> right now my user_id in the @recipe object is nil, not sure why...
+        @recipe.delete
+        flash[:message] = "recipe successfully deleted!"
+        redirect "/recipes"
+      else
+        flash[:errors] = "You can't delete other peoples recipes. Only your own."
+        redirect "/recipes"
+      end
+    else
+      flash[:errors] = "If you're a hacker, go away. Otherwise, you should log in."
+      rediret "/login"
+    end
+  end
 end
